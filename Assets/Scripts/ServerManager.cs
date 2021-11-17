c2f4d3b0f82febc4b1820a117654e4d4a5a45db3
CelestinML <celestin.moenne@gmail.com>
2021-11-17 11:37:10 -0500

    add call to statuslabel

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -6,45 +6,45 @@ using Unity.Netcode;
 public class ServerManager : MonoBehaviour
 {
     public GameObject m_serverButtons;
+    private bool m_modeSelected = false;
 
-    //void OnGUI()
-    //{
-    //    GUILayout.BeginArea(new Rect(50, 50, 800, 1600));
-    //    if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-    //    {
-    //        StartButtons();
-    //    }
-    //    else
-    //    {
-    //        StatusLabels();
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(50, 50, 800, 1600));
 
-    //        SubmitNewPosition();
-    //    }
+        if (m_modeSelected)
+        {
+            StatusLabels();
+        }
 
-    //    GUILayout.EndArea();
-    //}
+        GUILayout.EndArea();
+    }
 
     public void StartHost()
     {
+        m_modeSelected = true;
+        Cursor.lockState = CursorLockMode.Locked;
         m_serverButtons.SetActive(false);
         NetworkManager.Singleton.StartHost();
     }
 
     public void StartClient()
     {
+        m_modeSelected = true;
+        Cursor.lockState = CursorLockMode.Locked;
         m_serverButtons.SetActive(false);
         NetworkManager.Singleton.StartClient();
     }
 
-    //static void StatusLabels()
-    //{
-    //    var mode = NetworkManager.Singleton.IsHost ?
-    //        "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+    static void StatusLabels()
+    {
+        var mode = NetworkManager.Singleton.IsHost ?
+            "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
 
-    //    GUILayout.Label("Transport: " +
-    //        NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-    //    GUILayout.Label("Mode: " + mode);
-    //}
+        GUILayout.Label("Transport: " +
+            NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Mode: " + mode);
+    }
 
     //static void SubmitNewPosition()
     //{
