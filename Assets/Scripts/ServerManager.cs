e17b8c14fc1e1fe6689cb0b08d91cb9e4fa1c9b7
CelestinML <celestin.moenne@gmail.com>
2021-11-25 18:52:02 -0500

    [dev] start working on lobby

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -2,64 +2,134 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
-public class ServerManager : MonoBehaviour
+public class ServerManager : NetworkBehaviour
 {
-    public GameObject m_serverButtons;
-    private bool m_modeSelected = false;
+    [Header("UIs")]
+    public GameObject m_chooseModeUI;
+    public GameObject m_startGameUI;
+    public GameObject m_connectedPlayersUI;
 
-    private List<GameObject> m_connectedPlayers = new List<GameObject>();
+    [Header("Texts")]
+    public GameObject m_warningName;
+    public TMP_Text m_name;
+    
+    [Header("Games Infos")]
     public Transform m_spawnPositions;
-    private List<Transform> m_spawnPositionsList = new List<Transform>();
-    private List<int> m_freeSpawnPoints = new List<int>();
-
     public EnvironmentSpawner m_environmentSpawner;
     public CatManager m_catManager;
 
+    private List<TMP_Text> m_connectedPlayersTextBoxes = new List<TMP_Text>();
+    private TMP_Text m_playersNb;
+
+    private Dictionary<ulong, GameObject> m_connectedPlayers = new Dictionary<ulong, GameObject>();
+    private Dictionary<ulong, string> m_connectedPlayersNames = new Dictionary<ulong, string>();
+    private List<Transform> m_spawnPositionsList = new List<Transform>();
+    private List<int> m_freeSpawnPoints = new List<int>();
+
     private void Start()
     {
+        m_warningName.SetActive(false);
         for (int i = 0; i < 6; i++)
         {
             m_spawnPositionsList.Add(m_spawnPositions.GetChild(i));
             m_freeSpawnPoints.Add(i);
         }
+
+        for (int i = 1; i < 7; i++)
+        {
+            m_connectedPlayersTextBoxes.Add(m_connectedPlayersUI.transform.GetChild(i).GetComponent<TMP_Text>());
+            m_connectedPlayersTextBoxes[m_connectedPlayersTextBoxes.Count - 1].text = "";
+        }
+        m_playersNb = m_connectedPlayersUI.transform.GetChild(7).GetComponent<TMP_Text>();
+        m_playersNb.text = "";
     }
 
-    void OnGUI()
+    private void RefreshConnectedPlayersUI()
     {
-        GUILayout.BeginArea(new Rect(50, 50, 800, 1600));
-
-        if (m_modeSelected)
+        ResetPlayerNameUIClientRpc();
+        int i = 0;
+        foreach (string name in m_connectedPlayersNames.Values)
         {
-            StatusLabels();
+            AddPlayerNameToUIClientRpc(name, i);
+            i++;
         }
+    }
 
-        GUILayout.EndArea();
+    [ClientRpc]
+    void ResetPlayerNameUIClientRpc()
+    {
+        int i = 0;
+        foreach (string name in m_connectedPlayersNames.Values)
+        {
+            m_connectedPlayersTextBoxes[i].text = "";
+            i++;
+        }
+        m_playersNb.text = "0 / 6";
+    }
+
+    [ClientRpc]
+    void AddPlayerNameToUIClientRpc(string playerName, int i)
+    {
+        m_connectedPlayersTextBoxes[i].text = "- " + playerName;
+        m_playersNb.text = i + 1 + " / 6";
+    }
+
+    private bool CheckNickname()
+    {
+        if (m_name.text.Length <= 1)
+        {
+            m_warningName.SetActive(true);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void ButtonHost()
+    {
+        if (CheckNickname())
+        {
+            m_chooseModeUI.SetActive(false);
+            m_connectedPlayersUI.SetActive(true);
+            m_startGameUI.SetActive(true);
+            NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+            NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes(m_name.text);
+            if (NetworkManager.Singleton.StartHost()) 
+            {
+                RefreshConnectedPlayersUI();
+            }
+        }
+    }
+
+    public void ButtonClient()
+    {
+        if (CheckNickname())
+        {
+            m_chooseModeUI.SetActive(false);
+            m_connectedPlayersUI.SetActive(true);
+            //If we want to request a password
+            NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes(m_name.text);
+            if (NetworkManager.Singleton.StartClient())
+            {
+                RefreshConnectedPlayersUI();
+            }
+        }
     }
 
     public void StartHost()
     {
-        m_modeSelected = true;
         Cursor.lockState = CursorLockMode.Locked;
-        m_serverButtons.SetActive(false);
-
-        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
-
-        NetworkManager.Singleton.StartHost();
 
         m_environmentSpawner.InitializeEnvironmentClientRpc();
     }
 
     public void StartClient()
     {
-        m_modeSelected = true;
         Cursor.lockState = CursorLockMode.Locked;
-        m_serverButtons.SetActive(false);
-
-        //If we want to request a password
-        //NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes("room password");
-
-        NetworkManager.Singleton.StartClient();
 
         m_environmentSpawner.InitializeEnvironmentClientRpc();
     }
@@ -86,24 +156,20 @@ public class ServerManager : MonoBehaviour
         }
         else
         {
-            Debug.Log("Player approved");
-            int index = Random.Range(0, m_freeSpawnPoints.Count);
-            Transform spawnPosition = m_spawnPositionsList[m_freeSpawnPoints[index]];
-            m_freeSpawnPoints.RemoveAt(index);
-            Debug.Log("Position attributed : " + spawnPosition.position);
             //If approve is true, the connection gets added. If it's false. The client gets disconnected
-            callback(true, null, true, spawnPosition.position, spawnPosition.rotation);
+            callback(false, null, true, Vector3.zero, Quaternion.identity);
 
-            m_connectedPlayers.Add(NetworkManager.Singleton.ConnectedClientsList[NetworkManager.Singleton.ConnectedClientsList.Count - 1].PlayerObject.gameObject);
-            m_catManager.UpdatePlayerList(m_connectedPlayers);
+            string connectedPlayerName = System.Text.Encoding.ASCII.GetString(connectionData);
+            m_connectedPlayers.Add(clientId, null);
+            m_connectedPlayersNames.Add(clientId, connectedPlayerName);
         }
     }
     
     // Select a random player from the list that will become the cat for the beginning of the game
     public void SelectRandomCat()
     {
-        int index = Random.Range(0, m_connectedPlayers.Count);
-        m_catManager.SetPlayerAsCat(m_connectedPlayers[index]);
+        //int index = Random.Range(0, m_connectedPlayers.Count);
+        //m_catManager.SetPlayerAsCat(m_connectedPlayers[index]);
     }
 }
 
